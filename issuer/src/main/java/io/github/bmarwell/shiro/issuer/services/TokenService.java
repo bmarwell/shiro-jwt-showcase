29ca04eba5a351aa357d378ec8989465e9855798
Benjamin Marwell <bmarwell@apache.org>
2022-04-10 22:06:22 +0200

    [#8] add roles by query parameter (just for showcase).

--- issuer/src/main/java/io/github/bmarwell/shiro/issuer/services/TokenService.java
+++ issuer/src/main/java/io/github/bmarwell/shiro/issuer/services/TokenService.java
@@ -2,8 +2,9 @@ package io.github.bmarwell.shiro.issuer.services;
 
 import io.github.bmarwell.shiro.issuer.dto.LoginCredentials;
 import java.io.Serializable;
+import java.util.List;
 
 public interface TokenService extends Serializable {
 
-  String createJwt(LoginCredentials credentials);
+  String createJwt(LoginCredentials credentials, List<String> roles);
 }
