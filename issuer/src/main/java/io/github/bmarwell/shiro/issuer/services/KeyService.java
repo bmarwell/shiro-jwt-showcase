81fb76cfe9f5ba4e6014c4aa74b8cf5a49104164
Benjamin Marwell <bmarwell@apache.org>
2022-04-11 21:56:15 +0200

    [#26] extract keystore into module.

--- issuer/src/main/java/io/github/bmarwell/shiro/issuer/services/KeyService.java
+++ issuer/src/main/java/io/github/bmarwell/shiro/issuer/services/KeyService.java
@@ -1,15 +1,14 @@
 package io.github.bmarwell.shiro.issuer.services;
 
+import static java.util.Objects.requireNonNull;
+
 import io.github.bmarwell.shiro.issuer.json.JsonbSerializer;
+import io.github.bmarwell.shiro.keystore.KeystoreLoader;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.security.Key;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -20,30 +19,18 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class KeyService implements Serializable {
 
-  private static final String KEY_STORE_TYPE = "PKCS12";
-  private static final char[] KEY_STORE_PASSWORD = "changeit".toCharArray();
   private static final char[] KEY_PASSWORD = "changeit".toCharArray();
 
   @Inject
   private JsonbSerializer jsonbSerializer;
 
-  @Inject
-  @ConfigProperty(name="keystore.path")
-  File keystorePath;
-
   public KeyStore createKeyStore() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
-    KeyStore keyStore = KeyStore.getInstance(KEY_STORE_TYPE);
-
-    try (InputStream fis = Files.newInputStream(keystorePath.toPath(), StandardOpenOption.READ)) {
-      keyStore.load(fis, KEY_STORE_PASSWORD);
-    }
-
-    return keyStore;
+    final KeyStore keyStore = new KeystoreLoader().loadKeystore();
+    return requireNonNull(keyStore);
   }
 
   @Produces
@@ -54,6 +41,6 @@ public class KeyService implements Serializable {
 
     return Jwts.builder()
         .serializeToJsonWith(jsonbSerializer)
-        .signWith(signerKey, SignatureAlgorithm.ES256);
+        .signWith(requireNonNull(signerKey), SignatureAlgorithm.ES256);
   }
 }
