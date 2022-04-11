5b7cc6fa633b73ce8b207644c4b219ad67c0623d
Benjamin Marwell <bmarwell@apache.org>
2022-04-11 22:58:32 +0200

    [#22] add integration-tests for finish-server.

---
+++ finish/src/test/java/io/github/bmarwell/shiro/jwt/AbstractShiroJaxRsIT.java
@@ -0,0 +1,55 @@
+package io.github.bmarwell.shiro.jwt;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.github.bmarwell.shiro.jwt.json.JsonbConfigProvider;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import org.apache.johnzon.jaxrs.jsonb.jaxrs.JsonbJaxrsProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public abstract class AbstractShiroJaxRsIT {
+
+  final Client client = ClientBuilder.newClient()
+      .register(new JsonbConfigProvider())
+      .register(new JsonbJaxrsProvider<>());
+
+  protected abstract URI getBaseUri();
+
+  @BeforeEach
+  public void logOut() {
+  }
+
+
+  @Test
+  public void testGetUsersUnauthenticated() {
+    final WebTarget usersTarget = client.target(getBaseUri()).path("troopers");
+    final Response usersResponse = usersTarget.request(MediaType.APPLICATION_JSON_TYPE)
+        .buildGet()
+        .invoke();
+    assertThat(usersResponse.getStatus())
+        .isEqualTo(Status.UNAUTHORIZED.getStatusCode());
+  }
+
+  @Test
+  public void testGetUsersBasicAuthenticated() {
+    final WebTarget usersTarget = client.target(getBaseUri()).path("troopers");
+    final String basicToken = Base64.getEncoder().encodeToString("root:secret".getBytes(StandardCharsets.UTF_8));
+    final Response usersResponse = usersTarget.request(MediaType.APPLICATION_JSON_TYPE)
+        .header("Authorization", "Bearer " + basicToken)
+        .buildGet()
+        .invoke();
+    assertThat(usersResponse.getStatus())
+        .isEqualTo(Status.UNAUTHORIZED.getStatusCode());
+  }
+
+}
---
+++ finish/src/test/java/io/github/bmarwell/shiro/jwt/ShiroJwtJaxRsIT.java
@@ -0,0 +1,13 @@
+package io.github.bmarwell.shiro.jwt;
+
+import java.net.URI;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ShiroJwtJaxRsIT extends AbstractShiroJaxRsIT {
+
+  @Override
+  protected URI getBaseUri() {
+    return URI.create("http://localhost:" + System.getProperty("http.port") + "/" + System.getProperty("app.context.root"));
+  }
+}
