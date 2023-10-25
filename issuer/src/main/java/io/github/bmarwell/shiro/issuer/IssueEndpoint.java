8dd846acd776ca6c88e049f53fd91f6c9eaaedcf
Benjamin Marwell <bmarwell@apache.org>
2023-10-25 08:54:54 +0200

    [BUILD] add spotless

--- issuer/src/main/java/io/github/bmarwell/shiro/issuer/IssueEndpoint.java
+++ issuer/src/main/java/io/github/bmarwell/shiro/issuer/IssueEndpoint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The shiro-jjwt-showcase team
+ * Copyright (C) 2022-2023 The shiro-jjwt-showcase team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.github.bmarwell.shiro.issuer;
 
 import io.github.bmarwell.shiro.issuer.dto.LoginCredentials;
